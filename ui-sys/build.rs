07c86eb6454b6573ea5a72fd18a8d8052e4f5a02
Leonora Tindall <nora@nora.codes>
2019-05-08 18:18:39 -0500

    Build ui-sys with meson instead of cmake

--- ui-sys/build.rs
+++ ui-sys/build.rs
@@ -1,7 +1,4 @@
-extern crate cmake;
 extern crate bindgen;
-
-use cmake::Config;
 use bindgen::Builder as BindgenBuilder;
 
 use std::env;
@@ -11,12 +8,12 @@ use std::process::Command;
 fn main() {
     // Fetch the submodule if needed
     if cfg!(feature = "fetch") {
+        Command::new("git")
+            .args(&["version"])
+            .status()
+            .expect("Git does not appear to be installed. Error");
         // Init or update the submodule with libui if needed
         if !Path::new("libui/.git").exists() {
-            Command::new("git")
-                .args(&["version"])
-                .status()
-                .expect("Git does not appear to be installed. Error");
             Command::new("git")
                 .args(&["submodule", "update", "--init"])
                 .status()
@@ -35,37 +32,82 @@ fn main() {
         .opaque_type("max_align_t") // For some reason this ends up too large
         //.rustified_enum(".*")
         .generate()
-        .expect("Unable to generate bindings");
+        .expect("Unable to generate bindings. Error");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings");
+        .expect("Couldn't write bindings. Error");
 
     // Deterimine build platform
     let target = env::var("TARGET").unwrap();
     let msvc = target.contains("msvc");
     let apple = target.contains("apple");
 
+    let configured_out_dir = env::var("OUT_DIR").expect("Could not get OUT_DIR. Error");
     // Build libui if needed. Otherwise, assume it's in lib/
-    let mut dst;
+    let mut dst = Path::new(&configured_out_dir).join("build");
+    let build_destination: String = dst
+            .to_str()
+            .expect("Could not stringify build destination.")
+            .to_owned().clone();
     if cfg!(feature = "build") {
-        let mut cfg = Config::new("libui");
-        cfg.build_target("").profile("release");
-        if apple {
-            cfg.cxxflag("--stdlib=libc++");
-        }
-        dst = cfg.build();
+        // Verify that both meson and ninja are available
+        Command::new("meson")
+            .args(&["--version"])
+            .status()
+            .expect("Could not run meson. Error");
+        Command::new("ninja")
+            .args(&["--version"])
+            .status()
+            .expect("Could not run ninja. Error");
 
-        let mut postfix = Path::new("build").join("out");
-        if msvc {
-            postfix = postfix.join("Release");
+        // The directory where libui source is held.
+        let source_root_dir = "libui";
+        // The arguments to pass to meson
+        let mut args = vec![];
+
+        // If the build directory doesn't exist, create it
+        if !dst.exists() {
+            args.push("setup");
+        } else {
+            args.push("configure");
         }
-        dst = dst.join(&postfix);
+        // Push the build directory
+        args.push(&build_destination);
+
+        // Choose the type of library to build
+        if cfg!(feature = "static") {
+            args.push("--default-library=static");
+        } else {
+            args.push("--default-library=shared");
+        }
+
+        // Choose which optimization level to build
+        if cfg!(debug_assertions) {
+            args.push("--buildtype=debug");
+        } else {
+            args.push("--buildtype=release")
+        }
+
+        Command::new("meson")
+            .current_dir(source_root_dir)
+            .args(&args)
+            .status()
+            .expect("Could not configure build. Error");
+
+        Command::new("ninja")
+            .current_dir(&dst)
+            .status()
+            .expect("Could not build libui. Error");
+
+        dst = dst.join("meson-out");
+
+        if msvc {
+            dst = dst.join("Release");
+        }
     } else {
-        dst = env::current_dir()
-            .expect("Unable to retrieve current directory location.");
-        dst.push("lib");
+        dst = PathBuf::new().join("lib");
     }
 
     let libname;
@@ -76,5 +118,5 @@ fn main() {
     }
 
     println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib={}", libname);
+    println!("cargo:rustc-link-lib=dylib={}", libname);
 }
